6cdf4dd317713fd433079762c3b95ea5b27f9d3e
TalZa <24567980+TalZa1810@users.noreply.github.com>
2020-01-07 17:22:49 +0200

    staring with implementing interactive visual tests

--- src/ImageViewer/test/ImageViewer.visual.js
+++ src/ImageViewer/test/ImageViewer.visual.js
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { visualize, story, snap } from 'storybook-snapper';
 import ImageViewer from '..';
+import { imageViewerUniDriverFactory } from '../ImageViewer.uni.driver';
+import { uniTestkitFactoryCreator } from 'wix-ui-test-utils/vanilla';
+
+const dataHook = 'storybook-modal-preview-layout';
+
+const imageViewerUniTestkitFactory = uniTestkitFactoryCreator(
+  imageViewerUniDriverFactory,
+);
+
+const createDriver = () =>
+  imageViewerUniTestkitFactory({
+    wrapper: document.body,
+    dataHook,
+  });
 
 const imageUrl =
   'https://static.wixstatic.com/media/c78d05b79ede429fb77c9d8ec4443b93.jpg/v1/fit/w_375,h_375/c78d05b79ede429fb77c9d8ec4443b93.jpg';
@@ -52,16 +66,64 @@ const tests = [
       removeRoundedBorders: true,
     },
   },
+  {
+    it: 'Hover',
+    props: { imageUrl },
+    componentDidMount: async () => {
+      const driver = createDriver();
+      await driver.hover();
+    },
+  },
 ];
 
-const AsyncStoryWrapper = ({ onDone, ...rest }) => (
-  <ImageViewer onImageLoad={onDone} {...rest} />
-);
+const InteractiveImageViewer = ({ onDone, componentDidMount, ...props }) => {
+  const onImageLoad = () => {
+    componentDidMount && componentDidMount();
+    onDone();
+  };
+
+  return (
+    <ImageViewer dataHook={dataHook} onImageLoad={onImageLoad} {...props} />
+  );
+};
 
 visualize('ImageViewer', () => {
   story('should render', () => {
-    tests.forEach(({ it, props }) => {
-      snap(it, done => <AsyncStoryWrapper {...props} onDone={done} />);
+    tests.forEach(({ it, props, componentDidMount }) => {
+      snap(it, done => (
+        <InteractiveImageViewer
+          onDone={done}
+          componentDidMount={componentDidMount}
+          {...props}
+        />
+      ));
     });
   });
 });
+
+/*
+xdescribe('Overlay', () => {
+  it('should be visible while loading an image', async () => {
+    const { driver } = render(buildComponent({ imageUrl }));
+    expect(await driver.isOverlayVisible()).toBe(true);
+  });
+
+  it('should be visible while hovering an image', async () => {
+    const { driver } = render(buildComponent({ imageUrl }));
+    const privateDriver = imageViewerPrivateDriver({
+      element: await driver.element(),
+    });
+
+    await privateDriver.simulateImageLoad();
+    await driver.hover();
+
+    expect(await driver.isOverlayVisible()).toBe(true);
+  });
+
+  it('should NOT be visible when image is not currently loading and not being hovered', async () => {
+    const { driver } = render(buildComponent({ imageUrl: undefined }));
+    expect(await driver.isOverlayVisible()).toBe(false);
+  });
+});
+
+*/
